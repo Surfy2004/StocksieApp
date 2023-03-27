8a89ae0e90e2dc74e95ed32aa419519cb5363287
Surfy2004 <inesdimitrova28@gmail.com>
2023-03-27 20:59:33 +0300

    Add calculator fragment

--- app/src/main/java/com/example/stocksieapp/ui/CalculatorFragment.kt
+++ app/src/main/java/com/example/stocksieapp/ui/CalculatorFragment.kt
@@ -2,11 +2,16 @@ package com.example.stocksieapp.ui
 
 import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.TextView
 import com.example.stocksieapp.R
+import com.google.android.material.textfield.TextInputEditText
 
 class CalculatorFragment : Fragment() {
 
@@ -15,6 +20,8 @@ class CalculatorFragment : Fragment() {
     }
 
     private lateinit var viewModel: CalculatorViewModel
+    private lateinit var textEdit: EditText;
+    private lateinit var futureValue: TextView;
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,7 +32,22 @@ class CalculatorFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java)
+        viewModel = ViewModelProvider(this).get(CalculatorViewModel::class.java);
+        textEdit = view!!.findViewById(R.id.input_text);
+        futureValue = view!!.findViewById(R.id.future_value);
+        textEdit.addTextChangedListener(object : TextWatcher {
+
+            override fun afterTextChanged(s: Editable) {}
+
+            override fun beforeTextChanged(s: CharSequence, start: Int,
+                                           count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence, start: Int,
+                                       before: Int, count: Int) {
+                futureValue.setText(s);
+            }
+        });
         // TODO: Use the ViewModel
     }
 
